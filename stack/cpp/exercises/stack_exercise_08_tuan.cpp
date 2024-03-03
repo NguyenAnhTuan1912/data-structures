baad97d6db56daa0bb2219b1b7f888a0efa28c9b
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-03-03 18:06:10 +0700

    Update: exercise 8

--- stack/cpp/exercises/stack_exercise_08_tuan.cpp
+++ stack/cpp/exercises/stack_exercise_08_tuan.cpp
@@ -16,8 +16,38 @@
 #include <exception>
 #include <memory>
 #include <functional>
+#include <stdlib.h>
+#include <conio.h>
+#include <cmath>
+
+#define KEY_ESC 27
+#define KEY_u 117
+#define KEY_r 114
+#define KEY_i 105
+#define KEY_e 101
 
 using namespace std;
+
+enum StringAction {
+  Append,
+  Erase,
+  EMPTY
+};
+
+// State change of string
+class StringChange {
+public:
+  int atIndex = 0;
+  int amount = 0;
+  string data = "";
+  StringAction action = EMPTY;
+};
+
+class StringState {
+public:
+  string* curr = nullptr;
+  string prev = "";
+};
 
 template<class T>
 class StackNode {
@@ -38,6 +68,7 @@ class Stack {
 private:
   size_t __size = 0;
   size_t __max_size = 10;
+
   shared_ptr<StackNode<T>> __top = shared_ptr<StackNode<T>>(nullptr);
 
 public:
@@ -73,7 +104,6 @@ public:
     } catch(const exception& e) {
       cout << "Pop error: ";
       cerr << e.what();
-      return 0;
     }
   }
 
@@ -174,31 +204,125 @@ public:
     ptrA->data = move(ptrB->data);
     ptrB->data = move(data);
   }
+
+  size_t getSize() {
+    return this->__size;
+  }
+
+  void clear() {
+    this->__top = shared_ptr<StackNode<T>>(nullptr);
+    this->__size = 0;
+  }
 };
 
-template<class T>
-class HistoryStack {
+class StringHistoryStack {
 private:
-  Stack<T> __history_stack;
+  Stack<StringChange> __undo_stack;
+  Stack<StringChange> __redo_stack;
+  StringState __target;
 
 public:
-  HistoryStack(int s = 10) {
-    this->__history_stack = Stack<T>(s);
+  StringHistoryStack(string* t, int s = 10) {
+    this->__undo_stack = Stack<StringChange>(s);
+    this->__redo_stack = Stack<StringChange>(s);
+    this->__target.curr = t;
   };
 
   // redo
   void redo() {
+    // Get state
+    StringChange change = this->__redo_stack.pop();
+    // Do action here
+    if(change.action == Erase) {
+      *(this->__target.curr) = this->__target.curr->erase(change.atIndex, this->__target.curr->size());
+    }
 
+    if(change.action == Append) {
+      this->__target.curr->insert(change.atIndex, change.data);
+    }
+
+    // Push to undo
+    this->__undo_stack.push(change);
   }
 
   // undo
   void undo() {
+    // Get state
+    StringChange change = this->__undo_stack.pop();
 
+    // Save current state
+    this->__target.prev = *(this->__target.curr);
+
+    // Do action here
+    // Reverse action
+    if(change.action == Append) {
+      *(this->__target.curr) = this->__target.curr->erase(change.atIndex, this->__target.curr->size());
+    }
+
+    if(change.action == Erase) {
+      this->__target.curr->insert(change.atIndex, change.data);
+    }
+
+    // Push to redo
+    this->__redo_stack.push(change);
   }
 
-  // getCurrent
-  T getCurrent() {
-    this->__history_stack->getTop();
+  // startObserve
+  /**
+   * Use this method to start tracing the change of state of target, then
+   * generate the state of change for undo, redo action.
+   */
+  void startObserve() {
+    this->__target.prev = *(this->__target.curr);
+  }
+
+  // endObserve
+  /**
+   * Use this method to end tracing the change of state of target, then
+   * calculate change of targe.
+   */
+  void endObserve() {
+    int
+      N = this->__target.prev.size(),
+      M = this->__target.curr->size(),
+      amount = abs(M - N);
+
+    // Nothing change
+    if(N == M) return;
+
+    cout << "Previous size: " << N << endl;
+    cout << "Current size: " << M << endl;
+
+    StringChange change;
+    change.amount = amount;
+
+    // String is substracted
+    if(N > M) {
+      change.action = Erase;
+      change.data = this->__target.prev.substr(M, N);
+      change.atIndex = M;
+    }
+
+    // String is appended
+    if(N < M) {
+      change.action = Append;
+      change.data = this->__target.curr->substr(N, M);
+      change.atIndex = N;
+    }
+
+    cout << "Data: " << change.data << endl;
+
+    this->__undo_stack.push(change);
+    // Clear redo stack
+    this->__redo_stack.clear();
+  }
+
+  bool canUndo() {
+    return (this->__undo_stack.getSize() > 0);
+  }
+
+  bool canRedo() {
+    return (this->__redo_stack.getSize() > 0);
   }
 };
 
@@ -206,6 +330,97 @@ int main() {
   // Print Lambda
   ConstStackNodeCallBack<int> printValue = [](const shared_ptr<StackNode<int>>& sn) {
     cout << sn->data << " ";
+  };
+
+  // Apply for string
+  string* data = new string("");
+  string input = "";
+  int key = 0;
+
+  // Initialize StringHistoryStack
+  StringHistoryStack stringHistoryStack(data, 20);
+
+  while(true) {
+    system("cls");
+
+    // Choose options
+    cout << "Options:\n";
+    cout << "i - input string\n";
+    if(data->size() > 0) cout << "e - erase string\n";
+    if(stringHistoryStack.canUndo()) cout << "u - undo\n";
+    if(stringHistoryStack.canRedo()) cout << "r - redo\n";
+    cout << "=====\n";
+    cout << "esc - exit\n";
+    cout << "Press any (except above keys) key to continue\n";
+
+    key = getch();
+    // Check option
+    if(key == KEY_ESC) break;
+
+    system("cls");
+
+    switch(key) {
+      case KEY_i: {
+        // Start observe
+        stringHistoryStack.startObserve();
+
+        cout << "Nhap chuoi: \n";
+        cout << *(data);
+        getline(cin, input);
+        data->append(input);
+
+        system("cls");
+
+        // End observe
+        stringHistoryStack.endObserve();
+
+        break;
+      };
+
+      case KEY_e: {
+        // Start observe
+        stringHistoryStack.startObserve();
+        int s = 0, l = 0, N = data->size();
+
+        cout << "Xoa chuoi: \n";
+        cout << "Size: " << N << endl;
+        cout << *(data) << endl;
+        cout << "Bat dau: "; cin >> s;
+        cout << "Ket thuc: "; cin >> l;
+
+        if(s < 0) s = 0;
+        if(s >= N) s = N - 2;
+        if(l < s) l = s + 1;
+        if(l >= N) l = N - 1;
+
+        *data = data->erase(s, l);
+
+        // system("cls");
+
+        // End observe
+        stringHistoryStack.endObserve();
+        break;
+      };
+
+      case KEY_u: {
+        stringHistoryStack.undo();
+        break;
+      };
+
+      case KEY_r: {
+        stringHistoryStack.redo();
+        break;
+      };
+      default: break;
+    };
+
+    cout << "Text: \n";
+    cout << *data;
+    cout << "\n\n=====\n";
+    cout << "Press any (except above keys) key to continue\n";
+
+    // Check option
+    key = getch();
   };
 
   return 0;
