32d26e2e90ce7faa6759c60097357aa72f60678e
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-03-08 21:49:11 +0700

    Update: print method for Binary Search Tree

--- tree/tuan_cpp/examples/tree_example_01_tuan.cpp
+++ tree/tuan_cpp/examples/tree_example_01_tuan.cpp
@@ -7,7 +7,9 @@
 */
 
 #include <iostream>
+#include <vector>
 #include <functional>
+#include <iterator>
 #include <exception>
 
 using namespace std;
@@ -34,7 +36,7 @@ public:
 };
 
 template<class T>
-using ConstBSTNodeCallBack = function<void(const BSTNode<T>*)>;
+using ConstBSTNodeCallBack = function<void(const BSTNode<T>*m, int parentKey, int depth)>;
 
 template<class T>
 class BSTree {
@@ -44,9 +46,10 @@ private:
   int __depth = 0;
 
   static BSTNode<T>* __search(int key, BSTNode<T>* curr = nullptr);
-  static void __traverseInorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr);
-  static void __traversePreorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr);
-  static void __traversePostorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr);
+  static void __traverseInorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr, int parentKey, int d);
+  static void __traversePreorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr, int parentKey, int d);
+  static void __traversePostorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr, int parentKey, int d);
+  static void __insert(int key, T*& data, BSTNode<T>* curr, int& d, int& c);
 
 public:
   BSTree() = default;
@@ -60,41 +63,15 @@ public:
   };
 
   // Static methods
+  static bool isLeaf(const BSTNode<T>* curr);
   static bool isLeaf(BSTNode<T>* curr);
 
   // insertItem
-  void insertItem(int key, T* data, BSTNode<T>* curr = nullptr) {
-    try {
-      if(curr == nullptr) curr = this->__root;
-      if(curr->key == key) throw runtime_error("Cannot add this Node, because this key is exist before!!!");
-
-      bool isLeft = (key < curr->key);
-
-      // Assign to left
-      if(isLeft && curr->left == nullptr) {
-        curr->left = new BSTNode<T>(key, data);
-        this->__count++;
-        return;
-      };
-
-      // Assign to right
-      if(!isLeft && curr->right == nullptr) {
-        curr->right = new BSTNode<T>(key, data);
-        this->__count++;
-        return;
-      };
-
-      // Pass through left
-      if(isLeft) {
-        this->insertItem(key, data, curr->left);
-        return;
-      };
-
-      this->insertItem(key, data, curr->right);
-    } catch(const exception& e) {
-      cout << "Insert Error: ";
-      cerr << e.what();
-    };
+  void insertItem(int key, T* data) {
+    int d = 0, c = this->__count;
+    this->__insert(key, data, this->__root, d, c);
+    this->__count = c;
+    if(d > this->__depth) this->__depth = d;
   };
 
   // deleteItem
@@ -182,31 +159,149 @@ public:
 
   // traverse
   void traverse(ConstBSTNodeCallBack<T>& cb, TraverseType traverseType = Inorder) {
+    int d = 0;
     switch(traverseType) {
       case Preorder: {
-        this->__traversePreorder(cb, this->__root);
+        this->__traversePreorder(cb, this->__root, this->__root->key, d);
         break;
       };
 
       case Postorder: {
-        this->__traversePostorder(cb, this->__root);
+        this->__traversePostorder(cb, this->__root, this->__root->key, d);
         break;
       };
 
       case Inorder:
       default: {
-        this->__traverseInorder(cb, this->__root);
+        this->__traverseInorder(cb, this->__root, this->__root->key, d);
         break;
       };
     };
   };
 
+  // getDepth
   int getDepth() {
+    if(this->__root == nullptr) return 0;
     return this->__depth;
+  };
+
+  // print
+  void print() {
+    int D = this->__depth, maxOutputLength = 0;
+    vector<string> output((D + 1) * 2 - 1);
+
+    ConstBSTNodeCallBack<T> cb = [&](const BSTNode<T>* node, int parentKey, int depth) {
+      int index = depth * 2, M = 0, N = 0;
+      string dashes = "", spaces = "", subOutput = "", keyStr = to_string(node->key);
+      /*
+      cout << "Output: " << (output[index]) << endl;
+      cout << "Index: " << index << endl;
+      cout << "Key: " << keyStr << endl;
+      */
+      // Root of subtree
+      if(!this->isLeaf(node)) {
+        // Generate "-" for left child
+        if(node->left != nullptr) {
+          if(output[index + 1] != "") {
+            N = maxOutputLength - output[index + 1].size() - 1;
+            M = 2;
+          } else {
+            N = output[index + 2].size() - 1;
+            M = maxOutputLength - N;
+            M = M < 2 ? 2 : M;
+          };
+
+          dashes = string(N, ' ');
+          dashes += string(M, '-');
+
+          output[index + 1] += dashes;
+
+          maxOutputLength = output[index + 1].size() > maxOutputLength ? output[index + 1].size() : maxOutputLength;
+        }
+
+        // Add key string
+        N = maxOutputLength - output[index].size() - 1;
+
+        subOutput = string(N, ' ');
+        subOutput += keyStr;
+
+        output[index] += subOutput;
+
+        maxOutputLength = output[index].size() > maxOutputLength ? output[index].size() : maxOutputLength;
+
+        // Generate "-" for right child
+        N = 0, M = 0;
+        if(node->right != nullptr) {
+          if(output[index + 1] == "") {
+            N = maxOutputLength - 1;
+            M = to_string(node->right->key).size() + 1;
+          } else {
+            N = maxOutputLength - output[index + 1].size() - 1;
+            N = N < 0 ? 0 : N;
+            M = 2;
+          };
+
+          // M = maxOutputLength - output[index + 1].size();
+          // M = M < 2 ? 2 : M;
+
+          dashes = string(N, ' ');
+          dashes += string(M, '-');
+
+          output[index + 1] += dashes;
+
+          maxOutputLength = output[index + 1].size() > maxOutputLength ? output[index + 1].size() : maxOutputLength;
+        }
+      }
+      // Left
+      else if(node->key < parentKey) {
+        if(output[index] != "") {
+          N = maxOutputLength - output[index].size() - 1;
+          N = N == 0 ? 1 : N;
+        };
+
+        subOutput = string(N, ' ');
+        subOutput += keyStr;
+
+        output[index] += subOutput;
+
+        maxOutputLength = output[index].size() > maxOutputLength ? output[index].size() : maxOutputLength;
+      }
+      // Right
+      else if(node->key > parentKey) {
+        N = maxOutputLength - 1;
+
+        if(output[index] != "") {
+          N = maxOutputLength - output[index].size() - 1;
+          // cout << "Key: " << keyStr << endl;
+          // cout << "N: " << N << endl;
+        };
+
+        subOutput = string(N, ' ');
+        subOutput += keyStr;
+
+        output[index] += subOutput;
+
+        maxOutputLength = output[index].size() > maxOutputLength ? output[index].size() : maxOutputLength;
+      };
+    };
+
+    this->traverse(cb, Inorder);
+
+    typename vector<string>::iterator itr = output.begin();
+
+    while(itr != output.end()) {
+      cout << *itr << endl;
+      itr++;
+    };
   };
 };
 
 // Define static method
+template<class T>
+bool BSTree<T>::isLeaf(const BSTNode<T>* curr) {
+  return (curr->left == nullptr) && (curr->right == nullptr);
+};
+
 template<class T>
 bool BSTree<T>::isLeaf(BSTNode<T>* curr) {
   return (curr->left == nullptr) && (curr->right == nullptr);
@@ -227,56 +322,110 @@ BSTNode<T>* BSTree<T>::__search(int key, BSTNode<T>* curr) {
 };
 
 template<class T>
-void BSTree<T>::__traverseInorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr) {
+void BSTree<T>::__traverseInorder(
+  ConstBSTNodeCallBack<T>& cb,
+  BSTNode<T>* curr,
+  int parentKey,
+  int d
+) {
   if(curr == nullptr) return;
 
   if(curr->left != nullptr) {
-    __traverseInorder(cb, curr->left);
+    __traverseInorder(cb, curr->left, curr->key, d + 1);
   };
 
   // Execute callback
-  cb(curr);
+  cb(curr, parentKey, d);
 
   if(curr->right != nullptr) {
-    __traverseInorder(cb, curr->right);
+    __traverseInorder(cb, curr->right, curr->key, d + 1);
   };
 };
 
 template<class T>
-void BSTree<T>::__traversePreorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr) {
+void BSTree<T>::__traversePreorder(
+  ConstBSTNodeCallBack<T>& cb,
+  BSTNode<T>* curr,
+  int parentKey,
+  int d
+) {
   if(curr == nullptr) return;
 
   // Execute callback
-  cb(curr);
+  cb(curr, parentKey, d);
 
   if(curr->left != nullptr) {
-    __traversePreorder(cb, curr->left);
+    __traversePreorder(cb, curr->left, curr->key, d + 1);
   };
 
   if(curr->right != nullptr) {
-    __traversePreorder(cb, curr->right);
+    __traversePreorder(cb, curr->right, curr->key, d + 1);
   };
 };
 
 template<class T>
-void BSTree<T>::__traversePostorder(ConstBSTNodeCallBack<T>& cb, BSTNode<T>* curr) {
+void BSTree<T>::__traversePostorder(
+  ConstBSTNodeCallBack<T>& cb,
+  BSTNode<T>* curr,
+  int parentKey,
+  int d
+) {
   if(curr == nullptr) return;
 
   if(curr->left != nullptr) {
-    __traversePostorder(cb, curr->left);
+    __traversePostorder(cb, curr->left, curr->key, d + 1);
   };
 
   if(curr->right != nullptr) {
-    __traversePostorder(cb, curr->right);
+    __traversePostorder(cb, curr->right, curr->key, d + 1);
   };
 
   // Execute callback
-  cb(curr);
+  cb(curr, parentKey, d);
+};
+
+template<class T>
+void BSTree<T>::__insert(int key, T*& data, BSTNode<T>* curr, int& d, int& c) {
+  try {
+    if(curr == nullptr) {
+      d--;
+      return;
+    }
+    if(curr->key == key) throw runtime_error("Cannot add this Node, because this key is exist before!!!");
+
+    bool isLeft = (key < curr->key);
+
+    // Assign to left
+    if(isLeft && curr->left == nullptr) {
+      curr->left = new BSTNode<T>(key, data);
+      c++;
+      d++;
+      return;
+    };
+
+    // Assign to right
+    if(!isLeft && curr->right == nullptr) {
+      curr->right = new BSTNode<T>(key, data);
+      c++;
+      d++;
+      return;
+    };
+
+    // Pass through left
+    if(isLeft && curr->left != nullptr) BSTree<T>::__insert(key, data, curr->left, ++d, c);
+    else
+      if(!isLeft && curr->right != nullptr) BSTree<T>::__insert(key, data, curr->right, ++d, c);
+
+    return;
+  } catch(const exception& e) {
+    cout << "Insert Error: ";
+    cerr << e.what();
+  };
 };
 
 int main() {
   // Lambda
-  ConstBSTNodeCallBack<int> printValue = [](const BSTNode<int>* node) {
+  ConstBSTNodeCallBack<int> printValue = [](const BSTNode<int>* node, int parentKey, int depth) {
     cout << *(node->data) << " ";
   };
 
@@ -288,6 +437,9 @@ int main() {
   tree.insertItem(8, new int(8));
   tree.insertItem(10, new int(10));
   tree.insertItem(2, new int(2));
+  tree.insertItem(1, new int(1));
+
+  cout << "Depth of tree: " << tree.getDepth() << endl;
 
   cout << "Traverse tree (Inorder)\n";
   tree.traverse(printValue);
@@ -297,6 +449,11 @@ int main() {
   cout << endl;
   cout << "Traverse tree (Postorder)\n";
   tree.traverse(printValue, Postorder);
+  cout << endl;
+
+  // Print
+  cout << "Print tree: \n";
+  tree.print();
   cout << endl;
 
   cout << endl;
@@ -321,6 +478,27 @@ int main() {
   cout << endl;
   cout << "Traverse tree (Postorder)\n";
   tree.traverse(printValue, Postorder);
+  cout << endl;
+
+  // Print
+  cout << "Print tree: \n";
+  tree.print();
+  cout << endl;
+
+  tree = BSTree<int>(8, new int(8));
+
+  tree.insertItem(3, new int(3));
+  tree.insertItem(2, new int(2));
+  tree.insertItem(4, new int(4));
+  tree.insertItem(11, new int(11));
+  tree.insertItem(10, new int(10));
+  tree.insertItem(12, new int(12));
+  tree.insertItem(5, new int(5));
+  tree.insertItem(9, new int(9));
+
+  // Print
+  cout << "Print tree: \n";
+  tree.print();
   cout << endl;
 
   return 0;
